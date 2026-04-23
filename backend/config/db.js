1c4311e7b9815c94f107bfcf493a069ace4abb38
Mohammed Tawhid Mohiuddin <147172293+mdtawhid012@users.noreply.github.com>
2026-04-23 20:47:07 +0530

    init

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const logger = require('./logger');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://mohammedtawhid12:R6Tp7LZbQCunW9jH@task-manager.jkz8y.mongodb.net/copd_db');
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
         logger.info(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
